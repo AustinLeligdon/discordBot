f9b5826421d294edae61071524bd63eb363d0742
AustinKerr42 <akerr@iastate.edu>
2017-11-24 18:57:43 -0600

    Set up the basic bot. String messages are stored in another file. The discord token is stored in an untracked file called secret.py.

---
+++ messages.py
@@ -0,0 +1,6 @@
+#file of message variables
+Help = ['Welcome to the Discord Bot. List of commands:',\
+                '!help  -display the help command',\
+                '!test  -a test command for the bot',\
+                '!countdown  -time left until the next hackathon']
+Test = 'Test message'
---
+++ testBot.py
@@ -0,0 +1,41 @@
+import discord
+import asyncio
+from datetime import datetime
+import messages
+import secret
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('-------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!help'):
+        #Print out the possible commands for the bot
+        await client.send_message(message.channel, '\n'.join(messages.Help))
+    
+    elif message.content.startswith('!test'):
+        await client.send_message(message.channel, messages.Test)
+
+    elif message.content.startswith('!countdown'):
+        now = datetime.today()
+        hackerTime = datetime(2017, 12, 25, 17)
+        diff = hackerTime - now
+        time = ''
+
+        if(diff.days == 0):
+            hours = divmod(diff.seconds, 3600)[0]
+            time = 'There are ' + str(hours) + ' hours until HackISU 2018!'
+        elif(diff.days < 0):
+            time = 'It\'s happening!'
+        else:
+            time = 'There are ' + str(diff.days) + ' days until HackISU 2018!'
+
+        await client.send_message(message.channel, time)
+
+client.run(secret.Token)
