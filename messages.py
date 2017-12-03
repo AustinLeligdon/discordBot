d6e2f115b017f395cbd9c870fc2a472e83a7e90b
AustinKerr42 <akerr@iastate.edu>
2017-12-02 20:33:29 -0600

    Markdown in discord doesnt support links.

--- messages.py
+++ messages.py
@@ -1,27 +1,28 @@
 #file of message variables
-Help = '```Welcome to the Discord Bot. List of commands: \n'\
+Help = ''\
+    'Welcome to the Discord Bot. List of commands: \n'\
     '!cat  -conjure a random picture of a cat \n'\
     '!countdown  -time left until the next hackathon \n'\
     '!help  -display the help menu \n'\
     '!info  -display the info for HackISU 2018 \n'\
     '!social  -HackISU\'s social media accounts \n'\
     '!test  -a test command for the bot \n'\
-    '```'
+    ''
 
 Test = 'Test message'
 
-Info = '```'\
+Info = ''\
     'Location: Vermeer Applied Technology Hub/ISU Startup Factory \n'\
     'Address: 2710 S Loop Dr, Ames, IA 50010 \n'\
     'Start Time: 5pm (probably) \n'\
     'Website: <https://hackisu.org> \n'\
-    '```'
+    ''
 
-Social = '```'\
+Social = ''\
     'Twitter: <https://twitter.com/hackisu> \n'\
     'Facebook: <https://www.facebook.com/HackISU> \n'\
     'Instagram: <https://www.instagram.com/hack_isu> \n'\
     'Medium: <https://medium.com/@hackiowastate> \n'\
     'Slack: <https://hackisuviii.slack.com> \n'\
     'Discord: <https://discordapp.com/channels/377868588343754772/377868588343754774> \n'\
-    '```'
+    ''
